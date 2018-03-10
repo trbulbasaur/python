7bfcb71777338b0e5f3212494352e663a4109266
trbulbasaur <36964897+trbulbasaur@users.noreply.github.com>
2018-03-10 21:02:17 +0300

    Update hw2.py

--- Week2/hw2.py
+++ Week2/hw2.py
@@ -1,12 +1,12 @@
-kelime = input("Bir kelime giriniz: ")
-a = int(input("Baslangic sayisini giriniz: "))
-b = int(input("Sonlandirma sayisini giriniz(Kelimedeki harf sayısından büyük olmamalıdır.): "))
+kelime = input("Bir Kelime Giriniz : ")
+a = int(input("Başlangıç Sayısını Giriniz : "))
+b = int(input("Bitiş Sayısını Giriniz : "))
 if a > len(kelime) or b > len(kelime):
-    print("Kelimeden Büyük Değer Girilemez")
+    print("Kelimeden Büyük Değer Girilemez!")
 else:
-    if a == 0 or b ==0:
-        print("Sıfır Giremezsiniz")
+    if a <= 0 or b <= 0:
+        print("Sıfır Girilemez!")
     else:
         a = a-1
-        print(kelime)
-        print("{}".format(kelime[:a]+kelime[b:]))
+        print("Girdiğiniz Kelime : ", kelime)
+        print("Kesilmiş Kelime : {}".format(kelime[:a]+kelime[b:]))
